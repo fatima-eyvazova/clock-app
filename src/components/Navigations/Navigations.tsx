7276fe0ac1bbefe02d17e489dea73d1cbf0d09c8
fatime-eyvazova12 <fatima.eyvazovaa@gmail.com>
2024-09-04 09:52:55 +0400

    Feat (navigations:added navigations component)

---
+++ src/components/Navigations/Navigations.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { FaGlobe, FaBell, FaStopwatch, FaHourglassHalf } from "react-icons/fa";
+import "./Navigations.scss";
+
+const Navigations: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <nav className="navigation">
+      <ul>
+        <li className={location.pathname === "/world-clock" ? "active" : ""}>
+          <Link to="/world-clock">
+            <FaGlobe />
+            <span>World Clock</span>
+          </Link>
+        </li>
+        <li className={location.pathname === "/" ? "active" : ""}>
+          <Link to="/">
+            <FaBell />
+            <span>Alarm</span>
+          </Link>
+        </li>
+        <li className={location.pathname === "/stopwatch" ? "active" : ""}>
+          <Link to="/stopwatch">
+            <FaStopwatch />
+            <span>Stopwatch</span>
+          </Link>
+        </li>
+        <li className={location.pathname === "/timer" ? "active" : ""}>
+          <Link to="/timer">
+            <FaHourglassHalf />
+            <span>Timer</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigations;
